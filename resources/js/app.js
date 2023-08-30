c84a818366e00bcb37ee0868baecb027a6649e14
Raymark E. Dela Cruz <raymarkedelacruz@gmail.com>
2023-08-30 15:50:31 +0800

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+        return pages[`./Pages/${name}.vue`];
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(vuetify)
+            .mount(el);
+    },
+});
