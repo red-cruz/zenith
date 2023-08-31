a0a52e6c0424e8eabf4d781f26b92bab582f8af1
Raymark E. Dela Cruz <raymarkedelacruz@gmail.com>
2023-08-31 13:38:40 +0800

    update trycatch docs

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -17,16 +17,18 @@ class CartController extends Controller
 
     public function create(Request $request): JsonResponse
     {
-        return TryCatch::input(function () use ($request) {
+        return TryCatch::input(function () use ($request): JsonResponse {
             $validated = $request->validate([
               'product_id' => ['required', 'integer', 'exists:products,id'],
-              'quantity' => 'required|integer',
+              'quantity' => ['required', 'integer'],
             ]);
+
             $cart = new Cart();
             $cart->user_id = 1;
             $cart->product_id = $validated['product_id'];
             $cart->quantity = $validated['quantity'];
             $cart->save();
+
             return response()->json([
               'message' => 'Successfully added',
               'cart' => $cart
--- app/Http/Controllers/TryCatch.php
+++ app/Http/Controllers/TryCatch.php
@@ -3,20 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
+/**
+ * This class provides static functions to handle errors.
+ */
 class TryCatch extends Controller
 {
+    /**
+     * This function takes a callback function as an argument and tries to execute it.
+     * If the callback function throws an exception, the function will catch the exception and return a JSON response with the error message.
+     * If the callback function does not throw an exception, the function will return a JSON response with a success message.
+     *
+     * @param callable $callback The callback function to execute.
+     * @return JsonResponse The JSON response.
+     */
     public static function input(callable $callback): JsonResponse
     {
         try {
             return $callback();
         } catch(\Illuminate\Validation\ValidationException $err) {
+            /**
+             * This exception is thrown when the input data is invalid.
+             *
+             * @return JsonResponse The JSON response with the error message and the validation errors.
+             */
             return response()->json([
               'message' => 'Invalid input',
               'validation_errors' => $err->errors()
             ], 422);
         } catch (\Throwable $th) {
+            /**
+             * This exception is thrown for any other errors.
+             *
+             * @return JsonResponse The JSON response with the error message.
+             */
             return response()->json([
               'message' => 'Error: '.$th->getMessage()
             ], 500);
